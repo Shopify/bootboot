70c9a1a86cca6256011de2c4099d79d990289a31
Edouard CHIN <edouard.chin@shopify.com>
2018-11-15 04:02:33 +0100

    Cleanup Gem scaffold

--- lib/bootboot.rb
+++ lib/bootboot.rb
@@ -1,6 +1,4 @@
 require "bootboot/version"
 
 module Bootboot
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- test/bootboot_test.rb
+++ test/bootboot_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
 class BootbootTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Bootboot::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
