d3f16fa7c7301321ee73d5f08caec6fa7025d543
Jacob Smith <jacob@jacobsmith.io>
2020-02-25 15:54:51 -0500

    Run multiple Rubies and bundlers in Travis
    Using Travis' build matrix, this runs the tests using 3 versions of
    bundler and 4 versions of Ruby.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+ENV["BOOTBOOT_TEST_BUNDLER_VERSION"] ||= Bundler::VERSION
+puts "Running tests using Bundler #{ENV['BOOTBOOT_TEST_BUNDLER_VERSION']}"
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "bootboot"
 
